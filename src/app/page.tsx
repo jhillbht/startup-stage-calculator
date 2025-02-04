e64e2cbcec77925b6acba13b028e196c50c1b9e7
jhillbht <71741076+jhillbht@users.noreply.github.com>
2025-02-04 09:43:47 -0600

    Add main page component

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import StartupCalculator from '@/components/StartupCalculator';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-950 py-8">
+      <StartupCalculator />
+    </main>
+  );
+}
